a5b1ec21b99b74e794aaabba948fa249e733dec4
BishoyGeorgy <bishoy.georgy93@gmail.com>
2021-04-08 01:57:57 +0200

    Create backend folder | Add starter files

---
+++ spring-boot-ecommerce/src/main/java/com/ecommerce/ecommerce/entity/Product.java
@@ -0,0 +1,21 @@
+package com.ecommerce.ecommerce.entity;
+
+/**
+ * <p>
+ * Title: Product.java
+ * </p>
+ *
+ * <p>
+ * Description:
+ * </p>
+ *
+ * <p>
+ * Copyright: Copyright(c) , 2021
+ * </p>
+ *
+ * @author <a href="bishoy.georgy@incorta.com">Bishoy Georgy</a>
+ * @version 1.0
+ * @date 08/04/2021
+ */
+public class Product {
+}
